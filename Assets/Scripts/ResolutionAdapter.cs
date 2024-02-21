44224e8afc960adc9bcee18e33efe35966a8be53
Florian Mehou <72018820+FlorianHNQC@users.noreply.github.com>
2024-02-21 16:37:20 +0100

    feat(ResolutionAdapter.cs): Adapt boxcollider and rect scale

--- Assets/Scripts/ResolutionAdapter.cs
+++ Assets/Scripts/ResolutionAdapter.cs
@@ -3,40 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 [RequireComponent(typeof(RectTransform))]
 public class ResolutionAdapter : MonoBehaviour
 {
     private Image targetImage;
     private RectTransform imageRect;
+    private RectTransform prevImageRect;
     private Vector2 screenSize;
+
+    private BoxCollider boxCollider;
+    private BoxCollider2D boxCollider2D;
+    private float ratio;
 
     private void Awake()
     {
         screenSize = new Vector2(1170, 2532); //Default screen size (iphone12)
-       
+
         imageRect = GetComponent<RectTransform>();
+        boxCollider = GetComponent<BoxCollider>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
         if (screenSize.x != Screen.width || screenSize.y != Screen.height)
         {
-            ResizeImage();
-            screenSize = new Vector2(Screen.width, Screen.height);
+            ResizeRectTransform();
+            if (boxCollider != null) ResizeBoxCollider();
+            if (boxCollider2D != null) ResizeBoxCollider2D();
         }
     }
 
-    public void ResizeImage()
+    private void ResizeRectTransform()
     {
         Vector2 imagePos = imageRect.anchoredPosition;
+        Vector3 prevScale = imageRect.localScale;
 
-        float imageW = imageRect.rect.width;
-        float imageH = imageRect.rect.height;
+        float imageWPercent = imageRect.rect.width / screenSize.x;
+        float imageHPercent = imageRect.rect.height / screenSize.y;
 
-        float rap = imageW / imageH;
+        float rap = prevScale.x / prevScale.y;
 
-        float imageWPercent = imageW / screenSize.x;
-        float imageHPercent = imageH / screenSize.y;
+        float percentX = imagePos.x / screenSize.x;
+        float percentY = imagePos.y / screenSize.y;
 
-        float newWidth = Screen.width * imageWPercent;
-        float newHeight = Screen.height * imageHPercent;
+        float posX = Screen.width * percentX;
+        float posY = Screen.height * percentY;
+
+        float newScaleX = prevScale.x * Screen.width / screenSize.x;
+        float newScaleY = prevScale.y * Screen.height / screenSize.y;
+
+        float min = Mathf.Min(newScaleX, newScaleY);
+        if (min == newScaleX) {
+            newScaleY = (1 / rap) * newScaleX;
+        } else {
+            newScaleX = rap * newScaleY;
+        }
+
+        imageRect.anchoredPosition = new Vector2(posX, posY);
+        prevImageRect = imageRect;
+        imageRect.localScale = new Vector3(newScaleX, newScaleY, prevScale.z);
+    }
+
+    private void ResizeBoxCollider()
+    {
+        float colliderW = boxCollider.size.x;
+        float colliderH = boxCollider.size.y;
+
+        float rap = colliderW / colliderH;
+
+        float colliderWPercent = colliderW / prevImageRect.rect.width;
+        float colliderHPercent = colliderH / prevImageRect.rect.height;
+
+        float newWidth = imageRect.rect.width * colliderWPercent;
+        float newHeight = imageRect.rect.height * colliderHPercent;
 
         float min = Mathf.Min(newWidth, newHeight);
 
@@ -46,13 +82,31 @@ public class ResolutionAdapter : MonoBehaviour
             newWidth = rap * newHeight;
         }
 
-        float percentX = imagePos.x / screenSize.x;
-        float percentY = imagePos.y / screenSize.y;
-
-        float posX = Screen.width * percentX;
-        float posY = Screen.height * percentY;
-
-        imageRect.sizeDelta = new Vector2(newWidth, newHeight);
-        imageRect.anchoredPosition = new Vector2(posX, posY);
+        boxCollider.size = new Vector3(newWidth, newHeight, boxCollider.size.z);
     }
+
+    private void ResizeBoxCollider2D()
+    {
+        float colliderW = boxCollider2D.size.x;
+        float colliderH = boxCollider2D.size.y;
+
+        float rap = colliderW / colliderH;
+
+        float colliderWPercent = colliderW / prevImageRect.rect.width;
+        float colliderHPercent = colliderH / prevImageRect.rect.height;
+
+        float newWidth = imageRect.rect.width * colliderWPercent;
+        float newHeight = imageRect.rect.height * colliderHPercent;
+
+        float min = Mathf.Min(newWidth, newHeight);
+
+        if (min == newWidth) {
+            newHeight = (1 / rap) * newWidth;
+        } else {
+            newWidth = rap * newHeight;
+        }
+
+        boxCollider2D.size = new Vector2(newWidth, newHeight);
+    }
+
 }
